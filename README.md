4cf12f9b35a7aea58aeb428d65259e419468fd46
leblancr <rich.leblanc1@proton.me>
2024-08-22 03:00:14 -0400

    add vite config file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 to start:
 npx vite
+npx vite build
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/npm-experiment/', // Adjust to your GitHub Pages repository name
+});
